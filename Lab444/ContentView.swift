63fd621ca603235d105ce677e08e9647936fd689
romander333 <romander09@gmail.com>
2025-11-03 22:52:57 -0800

    Initial Commit

---
+++ Lab444/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab444
+//
+//  Created by ІПЗ-31/2 on 03.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab444/Lab444App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab444App.swift
+//  Lab444
+//
+//  Created by ІПЗ-31/2 on 03.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab444App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
